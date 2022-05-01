14cac5e2309e0797775f462aa3b61910dc4079e1
Atanas Vasilev <atanas.vasilev@gmail.com>
2022-05-01 10:52:04 +0300

    Change folder structure

--- exercism/go/zero-values/census/census.go
+++ exercism/go/zero-values/census/census.go
@@ -1,7 +1,6 @@
 // Package census simulates a system used to collect census data.
 package census
 
-// Resident represents a resident in this city.
 type Resident struct {
 	Name    string
 	Age     int
@@ -10,20 +9,38 @@ type Resident struct {
 
 // NewResident registers a new resident in this city.
 func NewResident(name string, age int, address map[string]string) *Resident {
-	panic("Please implement NewResident.")
+	return &Resident{
+		Name:    name,
+		Age:     age,
+		Address: address,
+	}
 }
 
 // HasRequiredInfo determines if a given resident has all of the required information.
 func (r *Resident) HasRequiredInfo() bool {
-	panic("Please implement HasRequiredInfo.")
+	street, keyExists := r.Address["street"]
+
+	if !keyExists || street == "" {
+		return false
+	}
+
+	return r.Age != 0
 }
 
 // Delete deletes a resident's information.
 func (r *Resident) Delete() {
-	panic("Please implement Delete.")
+	r.Name = ""
+	r.Age = 0
+	r.Address = nil
 }
 
 // Count counts all residents that have provided the required information.
 func Count(residents []*Resident) int {
-	panic("Please implement Count.")
+	rCount := 0
+	for _, r := range residents {
+		if r.HasRequiredInfo() {
+			rCount++
+		}
+	}
+	return rCount
 }
